efa06a1d7c2f7988db390862f1667a90d17649de
Adriano Mateus <azevedomateus.aam@gmail.com>
2019-07-09 19:23:58 -0300

    primeira lista

---
+++ src/main/java/com/example/cursomc/resouces/CategoriaResouces.java
@@ -0,0 +1,29 @@
+package com.example.cursomc.resouces;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value ="/categorias")
+public class CategoriaResouces {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+}
